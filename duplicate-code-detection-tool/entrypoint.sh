6518ab8befeb66847b2ef9ff430329841ba8a52f
mertkilicaslan <mkilicaslan@sabanciuniv.edu>
2023-04-11 18:49:17 +0300

    Clone duplicate code detection tool from external repository This commit clones a 'Duplicate Code Detection Tool' from an external repository and adds it to our project repository. The tool will be used to detect similar code fragments within software systems, helping us to identify and eliminate code clones. We will integrate this tool into our automated code clone detection framework as we progress

---
+++ duplicate-code-detection-tool/entrypoint.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+set -eu
+
+script_dir="$(dirname "$0")"
+cd $script_dir
+
+pull_request_id=$(cat "$GITHUB_EVENT_PATH" | jq 'if (.issue.number != null) then .issue.number else .number end')
+branch_name="pull_request_branch"
+
+if [ $pull_request_id == "null" ]; then
+  echo "Could not find a pull request ID. Is this a pull request?"
+  exit 1
+fi
+
+maintainer=${GITHUB_REPOSITORY%/*}
+eval git clone "https://${maintainer}:${INPUT_GITHUB_TOKEN}@github.com/${GITHUB_REPOSITORY}.git" ${GITHUB_REPOSITORY}
+cd $GITHUB_REPOSITORY
+eval git config remote.origin.fetch +refs/heads/*:refs/remotes/origin/*
+eval git fetch origin pull/$pull_request_id/head:$branch_name
+eval git checkout $branch_name
+
+latest_head=$(git rev-parse HEAD)
+
+eval python3 /action/run_action.py --latest-head $latest_head --pull-request-id $pull_request_id
