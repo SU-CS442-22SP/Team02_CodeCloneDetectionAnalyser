c9c16e50cb2ef2bcb71c53722618951a91a39929
Mert Kilicaslan <mkilicaslan@sabanciuniv.edu>
2023-05-30 16:37:59 +0300

    framework now gives the merged results of the tools framework util py has been updated so that, now it merges the resulted pair files from type1 and type2 cc detection tools. It creates overall results folder, and writes a merged version of the had compared each file pair

--- framework_util.py
+++ framework_util.py
@@ -1,7 +1,8 @@
 import os
 import argparse
 
-def simpleCC_Merger():
+
+def simpleCC_output_generator():
     file_content = {}
     with open('Type2_SimpleCC_All_Pairs.txt', 'r') as file:
         data = file.read().split('</pair>')
@@ -23,8 +24,9 @@ def simpleCC_Merger():
             if file2_name == file1_name:
                 continue
 
-            # Generate file name as 'file_num1-file_num2.txt' ensuring consistent order
-            file_name = '-'.join(sorted([file1_name, file2_name])) + 'Type2.txt'
+            # Generate file name as 'file_num1-file_num2_simpleCC_results.txt' ensuring consistent order
+            file_name = '-'.join(sorted([file1_name, file2_name])
+                                 ) + '_simpleCC_results.txt'
 
             # If file already exists in the dictionary, append the new content. Otherwise, create a new entry
             if file_name in file_content:
@@ -40,14 +42,62 @@ def simpleCC_Merger():
     # Write the data to respective files
     for file_name, contents in file_content.items():
         with open(os.path.join('Type2_SimpleCC_Results', file_name), 'w') as f:
+            # Write the file title as the first line, remove "_simpleCC_results" from the header
+            f.write('SimpleCC Results for the files: ' +
+                    file_name.replace('_simpleCC_results.txt', '').replace('-', ' - ') + '\n\n')
             for content in contents:
                 f.write(content + '\n')
 
+
+def overall_cc_output_generator():
+    # Directories to merge files from
+    dir1 = 'Type1_CC_Results'
+    dir2 = 'Type2_SimpleCC_Results'
+
+    # Check if directories exist
+    if not os.path.exists(dir1) or not os.path.exists(dir2):
+        print(f"Both directories '{dir1}' and '{dir2}' must exist.")
+        return
+
+    # Get file names from both directories
+    dir1_files = os.listdir(dir1)
+    dir2_files = os.listdir(dir2)
+
+    # Transform file names for matching
+    dir1_files_transformed = {
+        f.replace('_results.txt', ''): f for f in dir1_files}
+    dir2_files_transformed = {
+        f.replace('_simpleCC_results.txt', ''): f for f in dir2_files}
+
+    # Create output directory if it doesn't exist
+    out_dir = 'Total_CC_Result'
+    if not os.path.exists(out_dir):
+        os.makedirs(out_dir)
+
+    # For each file in dir2, check if it exists in dir1 and merge them
+    for file_transformed, file_original in dir2_files_transformed.items():
+        if file_transformed in dir1_files_transformed:
+            # Open the output file in write mode
+            with open(os.path.join(out_dir, file_transformed + '_total_cc_result.txt'), 'w') as outfile:
+                # Write contents of file from dir1
+                with open(os.path.join(dir1, dir1_files_transformed[file_transformed]), 'r') as infile:
+                    outfile.write(infile.read())
+
+                outfile.write('\n' + '-'*200 + '\n')
+
+                # Write contents of file from dir2
+                with open(os.path.join(dir2, file_original), 'r') as infile:
+                    outfile.write(infile.read())
+
+
 def main(arg):
-    if(arg == 1):
-        simpleCC_Merger()
+    if (arg == 1):
+        simpleCC_output_generator()
+    elif arg == 2:
+        overall_cc_output_generator()
     else:
         print("Invalid argument value")
+
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
--- run_scripts.bat
+++ run_scripts.bat
@@ -37,36 +37,33 @@ echo.
 set /p "choice=Choice: "
 
 :: Check the user's input
-
-:: Run duplicate code detector 
 if "!choice!"=="1" (
+    :: Run duplicate code detector Type1
     set /p IGNORE_THRESHOLD="Please provide the ignore threshold for Duplicate Code Detection: "
     python -W ignore "%PROJECT_PATH%%TOOL_1_NAME%duplicate_code_detection.py" --ignore-threshold !IGNORE_THRESHOLD! --csv-output Type1_CC_Percentage.txt -d "%PROJECT_PATH%%DATA_SET_NAME%"
 
 ) else if "!choice!"=="2" (
-    :: Run SimpleCC
+    :: Run SimpleCC Type2
     java -cp "%ANTLR4_PATH%;%PROJECT_PATH%%TOOL_2_NAME%simplecc.jar" jp.naist.se.simplecc.CloneDetectionMain "%PROJECT_PATH%%DATA_SET_NAME%" > Type2_SimpleCC_All_Pairs.txt
     python framework_util.py 1
     
 ) else if "!choice!"=="3" (
-    :: Compile and run myCC
+    :: Run myCC Type2
     set FILE1=%PROJECT_PATH%%TOOL_3_NAME%\dataset\file1.java
     set FILE2=%PROJECT_PATH%%TOOL_3_NAME%\dataset\file2.java
 
     java -jar "%PROJECT_PATH%%TOOL_3_NAME%target\Type2CodeCloneDetection-1.0-SNAPSHOT.jar" "!FILE1!" "!FILE2!"
     
 ) else if "!choice!"=="4" (
-    :: Run duplicate code detector 
+    :: Duplicate code detector Type1
     set /p IGNORE_THRESHOLD="Please provide the ignore threshold for Duplicate Code Detection: "
     python -W ignore "%PROJECT_PATH%%TOOL_1_NAME%duplicate_code_detection.py" --ignore-threshold !IGNORE_THRESHOLD! --csv-output Type1_CC_Percentage.txt -d "%PROJECT_PATH%%DATA_SET_NAME%"
 
+    :: SimpleCC Type2
     java -cp "%ANTLR4_PATH%;%PROJECT_PATH%%TOOL_2_NAME%simplecc.jar" jp.naist.se.simplecc.CloneDetectionMain "%PROJECT_PATH%%DATA_SET_NAME%" > Type2_SimpleCC_All_Pairs.txt
-    python framework_util.py
+    python framework_util.py 1
+    python framework_util.py 2
 
-    set FILE1=%PROJECT_PATH%%TOOL_3_NAME%\dataset\file1.java
-    set FILE2=%PROJECT_PATH%%TOOL_3_NAME%\dataset\file2.java
-
-    java -jar "%PROJECT_PATH%%TOOL_3_NAME%target\Type2CodeCloneDetection-1.0-SNAPSHOT.jar" "!FILE1!" "!FILE2!"
 ) else (
     exit
 )
