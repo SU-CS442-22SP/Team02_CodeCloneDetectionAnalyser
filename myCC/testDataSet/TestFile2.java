438220d3e7f0aaf1720818c8ad060047e58646e3
Mert Kilicaslan <mkilicaslan@sabanciuniv.edu>
2023-06-12 21:42:09 +0300

    Final version of the framework Final version of the framework

---
+++ myCC/testDataSet/TestFile2.java
@@ -0,0 +1,11 @@
+public class TestFile2 {
+    public void addition(int a, int b) {
+        int result = a + b;
+        System.out.println("The result is " + result);
+    }
+
+    public void subtraction(int a, int b) {
+        int result = a - b;
+        System.out.println("The result is " + result);
+    }
+}
